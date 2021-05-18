21f5eb5e7ff31f99a59eca2f0c65e5c5258cbd97
Icelk <main@icelk.dev>
2021-05-18 06:10:48 +0200

    Added logo prototype.

---
+++ logo/download.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+echo "I use PureRef to view these images."
+
+# https://varoguiden.se/sunvarakvarn.html
+curl https://varoguiden.se/images/sunvarakvarn-puff.png -o sundvära.png
+# https://www.bildblogg.cavok.se/2014/07/revinge-kvarn/
+curl https://www.bildblogg.cavok.se/wp-content/uploads/2014/07/revinge-kvarn-1279.jpg -o revinge.jpg
+# https://www.hornchurchandupminsterconservatives.com/news/upminster-windmill-restoration
+curl https://www.hornchurchandupminsterconservatives.com/sites/www.hornchurchandupminsterconservatives.com/files/styles/gallery_large/public/upminster_windmill.jpg -o upminster.jpg
+# https://topwindmill.blogspot.com/2019/05/windmill-westhoughton.html
+# under "Lytham Windmill Lancashire England Lovely Old Windmill Museum"
+curl https://media2.trover.com/T/5d6f90266e1f1f1f5a002e6f/fixedw_large_4x.jpg -o westhoughton.jpg
