3576ebf1c1663a26e8c76354542318b6e919fe33
Icelk <main@icelk.dev>
2021-05-28 23:24:40 +0200

    Search button now works.

---
+++ public/script.js
@@ -0,0 +1,64 @@
+const initSmoothScrolling = () => {
+    document.querySelectorAll('a[href^="#"]').forEach((anchor_link) => {
+        anchor_link.addEventListener("click", function (e) {
+            e.preventDefault()
+
+            let anchor = document.getElementById(
+                this.getAttribute("href").substring(1)
+            )
+
+            if (anchor !== null) {
+                anchor.scrollIntoView({
+                    behavior: "smooth",
+                })
+            }
+        })
+    })
+}
+const initCopyHeading = () => {
+    // Only enable on
+    let metaEnabled = document.querySelector("meta[name='permalinks']")
+    let content = metaEnabled?.getAttribute("content")
+    if (content !== "enabled" && content !== "not-titles") {
+        return
+    }
+
+    let queryString =
+        content === "not-titles"
+            ? "h2, h3, h4, h5, h6"
+            : "h1, h2, h3, h4, h5, h6"
+
+    document.querySelectorAll(queryString).forEach((heading) => {
+        let id = heading.getAttribute("id")
+        if (id !== undefined) {
+            let linkButton = document.createElement("span")
+            linkButton.title = "Copy permalink"
+            linkButton.innerHTML =
+                '<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">\
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 5H6a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2v-1M8 5a2 2 0 002 2h2a2 2 0 002-2M8 5a2 2 0 012-2h2a2 2 0 012 2m0 0h2a2 2 0 012 2v3m2 4H10m0 0l3-3m-3 3l3 3" />\
+            </svg>'
+            let linkSVG = linkButton.firstElementChild
+            linkButton.onclick = () => {
+                let link = `${document.location.href.split("#")[0]}#${id}`
+                navigator.clipboard.writeText(link)
+                linkSVG.classList.add("pressed")
+                setTimeout(() => linkSVG.classList.remove("pressed"), 150)
+            }
+            linkSVG.classList.add("share-button")
+            heading.appendChild(linkButton)
+        }
+    })
+}
+
+initSmoothScrolling()
+initCopyHeading()
+
+let search = document.getElementById("searchInput")
+search.addEventListener("keydown", (e) => {
+    if (e.key !== "Enter") {
+        return
+    }
+    let input = search.value
+    let percentEncoded = encodeURIComponent(input)
+    location.href = `https://duckduckgo.com/?q=site%3Akvarn.org+${percentEncoded}`
+})
