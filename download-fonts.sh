a272acfd805b0b1ee0f664d5a848bfb402d8c275
Icelk <main@icelk.dev>
2023-01-06 22:40:33 +0100

    Update font download script.

--- download-fonts.sh
+++ download-fonts.sh
@@ -2,7 +2,7 @@
 
 n="kurale"
 
-curl "https://google-webfonts-helper.herokuapp.com/api/fonts/kurale?download=zip&subsets=latin-ext,latin&variants=regular&formats=woff,woff2" -o "$n.zip"
+curl "https://gwfh.mranftl.com/api/fonts/kurale?download=zip&subsets=latin-ext,latin&variants=regular&formats=woff,woff2" -o "$n.zip"
 
 mkdir "$n"
 unzip "$n.zi"p -d "$n"
@@ -14,7 +14,7 @@ rm "$n.zip"
 
 n="cairo"
 
-curl "https://google-webfonts-helper.herokuapp.com/api/fonts/cairo?download=zip&subsets=latin-ext,latin&variants=300,700&formats=woff,woff2" -o "$n.zip"
+curl "https://gwfh.mranftl.com/api/fonts/cairo?download=zip&subsets=latin-ext,latin&variants=300,700&formats=woff,woff2" -o "$n.zip"
 
 mkdir "$n"
 unzip "$n.zip" -d "$n"
